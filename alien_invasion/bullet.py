a6be1aa4d09e17e02b3eec26076e0e6cd8a25eb5
maplexuci <zhhxu9@gmail.com>
2019-12-20 15:48:01 -0500

    update() and draw_bullet() functions added to Bullet class

--- alien_invasion/bullet.py
+++ alien_invasion/bullet.py
@@ -13,9 +13,18 @@ class Bullet(Sprite):
         self.color = self.settings.bullet_color
 
         # Create a bullet rect at (0, 0) and then set correct position.
-        self.rect = pygame.Rect(0, 0, self.settings.bullet_width, 
-            self.settings.bullet_height)
+        self.rect = pygame.Rect(0, 0, self.settings.bullet_width, self.settings.bullet_height)
         self.rect.midtip = ai_game.ship.rect.midtop
 
         # Store the bullet's position as a decimal value
         self.y = float(self.rect.y)
+    def update(self):
+        """Move the bullet up the screen."""
+        # Update the decimal position of the bullet.
+        self.y -= self.settings.bullet_speed
+        # Update the rect position.
+        self.rect.y = self.y
+
+    def draw_bullet(self):
+        """Draw the bullet to the screen."""
+        pygame.draw.rect(self.screen, self.color, self.rect)
